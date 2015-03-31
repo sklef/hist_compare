9cbc69e2a6bb433f79367e3f893d55b40dc712fe
Dmitry Smolyakov <d.s.smolyakov@gmail.com>
2015-03-31 23:13:36 +0300

    Fixed structure of output

--- client.py
+++ client.py
@@ -7,8 +7,18 @@ def hist_compare(first_file, second_file, all_paths):
     all_pvalues = []
     for path in all_paths:
         payload = {'base_hist': first_file , 'cur_hist': second_file, 'paths': path }
-        new_pvalue = float(requests.get(HOST_NAME, params=payload).text)
-        all_pvalues.append(new_pvalue)
+        result = requests.get(HOST_NAME, params=payload).text
+        message = ""
+        try:
+            distance = float(result)
+            rc = 0
+            struct = {'rc': rc, 'distance': distance, 'message': message}
+            all_pvalues.append(struct)
+        except ValueError:
+            message = result
+            rc  = 1
+            struct = {'rc': rc, 'message': message}
+            all_pvalues.append(struct)
     return all_pvalues
 
 if __name__ == '__main__':
