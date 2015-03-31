3e8c1bd13a441777c0c0b7b987f40fac4cca9ffb
Dmitry Smolyakov <d.s.smolyakov@gmail.com>
2015-03-31 22:51:44 +0300

    Add exception check

--- api.py
+++ api.py
@@ -6,14 +6,17 @@ app = Flask(__name__)
 
 @app.route('/compare')
 def compare():
-    base_hist = request.args['base_hist']
-    cur_hist = request.args['cur_hist']
-    all_paths = request.args['paths']
-    base_file = root_open(base_hist)
-    cur_file = root_open(cur_hist)
-    cur_hist = cur_file.get(all_paths.encode('ascii','ignore'))
-    base_hist = cur_file.get(all_paths.encode('ascii','ignore'))
-    s = cur_hist.KolmogorovTest(base_hist)
+    try:
+        base_hist = request.args['base_hist']
+        cur_hist = request.args['cur_hist']
+        all_paths = request.args['paths']
+        base_file = root_open(base_hist)
+        cur_file = root_open(cur_hist)
+        cur_hist = cur_file.get(all_paths.encode('ascii','ignore'))
+        base_hist = cur_file.get(all_paths.encode('ascii','ignore'))
+        s = cur_hist.KolmogorovTest(base_hist)
+    except Exception, s:
+        return s
     return str(s)
 
 if __name__ == '__main__':
