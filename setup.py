2827f59384e786724a39552802b4afdbad305527
dsmolyakov <d.s.smolyakov@gmail.com>
2015-05-25 16:11:23 +0300

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-__author__ = 'dsmolyakov'
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hist_compare',
+    url='https://github.com/sklef/hist_compare',
+    author='Dmitry Smolyakov',
+    version='0.1.0',
+    author_email='dsmolyakov@yandex-team.ru',
+    packages=find_packages(),
+    description='Service for comparing particle histograms',
+    install_requires=[
+        "Flask==0.10.1",
+        "Jinja2==2.7.3",
+        "wsgiref==0.1.2",
+        "flask-admin==1.1.0",
+        "SQLAlchemy==1.0.0b5",
+        "flask-sqlalchemy==2.0",
+        "requests==2.5.2",
+        "rootpy"
+    ],
+)
