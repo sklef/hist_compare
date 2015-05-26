d75c4c957a1cb73ba21b75c196579304eac9ac8f
dsmolyakov <d.s.smolyakov@gmail.com>
2015-05-26 12:21:19 +0300

    Remove flask login integration

--- hist_compare/db_models.py
+++ hist_compare/db_models.py
@@ -12,20 +12,6 @@ class User(db.Model):
     email = db.Column(db.String(120))
     password = db.Column(db.String(64))
 
-    # Flask-Login integration
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.id
-
-    # Required for administrative interface
     def __unicode__(self):
         return self.first_name
 
