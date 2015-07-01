def6ce88e7c2738a18d93b4eec562dc664e53972
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-07-01 09:34:54 +0200

    listneing to all interfaces

--- hist_compare/api.py
+++ hist_compare/api.py
@@ -35,4 +35,4 @@ if __name__ == '__main__':
     if not os.path.exists(database_path):
         utils.build_sample_db()
     # Start app
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
