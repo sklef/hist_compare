8abb9ac48f73a4394498092e8d7db1d6c44df943
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-07-01 13:48:47 +0200

    env path

--- test/run_client.sh
+++ test/run_client.sh
@@ -1,4 +1,4 @@
-[ `which python` != "~/virtual_env/bin/python" ] && source ~/virtual_env/bin/activate
+[ `which python` != "/home/austyuzh/virtual_env/bin/python" ] && source /home/austyuzh/virtual_env/bin/activate
 export https_proxy=
 export http_proxy=
 python test_client.py
